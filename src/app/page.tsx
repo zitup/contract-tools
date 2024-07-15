da8a5c9d9a729364a1187e7e1c29425f6dc9e82b
Z <yitongzhao@163.com>
2024-07-15 18:59:05 +0800

    feat: add contribution link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,19 @@ import DecodeFunctionData from '@/components/tools/DecodeFunctionData';
 import OperateUnverifiedContract from '@/components/tools/OperateUnverifiedContract';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { useAccount } from 'wagmi';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
 
 export default function Home() {
-  const { address } = useAccount();
+  const { theme } = useTheme();
+
   return (
     <main className="flex min-h-screen flex-col items-center ">
-      <header className="flex items-center self-end h-14 px-5">
-        {!!address && <ConnectButton />}
+      <header className="flex items-center self-end h-14 px-5 gap-2">
+        <ConnectButton />
         <ModeToggle />
       </header>
-      <Tabs defaultValue="decodeFunctionData" className="w-[80%]">
+      <Tabs defaultValue="decodeFunctionData" className="w-[80%] flex-1">
         <TabsList>
           <TabsTrigger value="decodeFunctionData">Decode Function Data</TabsTrigger>
           <TabsTrigger value="operateUnverifiedContract">Operate Unverified Contract</TabsTrigger>
@@ -26,6 +28,11 @@ export default function Home() {
           <OperateUnverifiedContract />
         </TabsContent>
       </Tabs>
+      <footer className="text-center py-4">
+        <a href="https://github.com/zitup/contract-tools">
+          <Image src={theme === 'light' ? 'github-mark.svg' : 'github-mark-white.svg'} width={20} height={20} alt="github" />
+        </a>
+      </footer>
     </main>
   );
 }
